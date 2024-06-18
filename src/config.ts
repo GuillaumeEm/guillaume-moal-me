e099629958d98ea36a0a2dfaa495bf4d20579bee
Guillaume M <guillaume@nuaj.eu>
2024-06-18 16:21:27 +0200

    Init GME

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Guillaume Moal - Freelance DevOps & Backend Engineer';
+export const SITE_DESCRIPTION = 'Portfolio & Curriculum of Guillaume Moal, freelance DevOps & SWE based in France';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
