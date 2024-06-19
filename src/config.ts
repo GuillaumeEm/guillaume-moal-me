cd8c7412c0606c8538637495d6e98960ff271359
Guillaume M <guillaume@nuaj.eu>
2024-06-19 09:38:44 +0200

    Seo, lang, cleaning

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Guillaume Moal - Freelance DevOps & Backend Engineer';
-export const SITE_DESCRIPTION = 'Portfolio & Curriculum of Guillaume Moal, freelance DevOps & SWE based in France';
+export const SITE_DESCRIPTION = 'Portfolio & Curriculum de Guillaume Moal, freelance DevOps & Backend Engineer basé en France';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
